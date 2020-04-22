b539a0fd5f88b355740078c6dd65ec329470d6ef
Léu Almeida <leo@webid.net.br>
2020-04-22 14:23:58 -0300

    feat(index): implementing mapview

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import MapView from 'react-native-maps';
 import { View } from 'react-native';
 
 // import { Container } from './styles';
 
 export default function src() {
-  return <View />;
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        region={{
+          latitude: -27.210753,
+          longitude: -49.644183,
+          latitudeDelta: 0.0143,
+          longitudeDelta: 0.0134,
+        }}
+        showsUserLocation
+        loadingEnabled
+      />
+    </View>
+  );
 }
