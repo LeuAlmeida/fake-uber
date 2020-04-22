91f0ffaebdc284207744ff3984cb9c2a8aa023f1
Léu Almeida <leo@webid.net.br>
2020-04-22 17:42:18 -0300

    chore(Search): remove eslint rule

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import { Platform } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
